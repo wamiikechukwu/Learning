d3e29149ad85ee20c9379f7bbee19603f3120bae
wamiikechukwu <wamiikechukwu@gmail.com>
2020-07-06 19:13:44 +0100

    Unit Test for the RegistrationTask.kt Passed

--- app/src/test/java/com/me/registrationtask/RegistrationTaskTest.kt
+++ app/src/test/java/com/me/registrationtask/RegistrationTaskTest.kt
@@ -36,7 +36,8 @@ class RegistrationTaskTest {
         registrationTask = RegistrationTask(userRepository)
     }
 
-    @Test()
+    @Test(expected = IllegalArgumentException::class)
+    //
     fun shouldThrowAnExceptionWhenUserNameIsEmpty() {
         val testUserName = fakeUserEntity
         testUserName.Username = ""
